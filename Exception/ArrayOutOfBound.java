a035595d3b144d2aa67c5d377b2d5e3462538922
psgaikwad2003 <psgaikwad2003@gmail.com>
2026-07-30 22:18:28 +0530

    Exception

---
+++ Exception/ArrayOutOfBound.java
@@ -0,0 +1,14 @@
+public class ArrayOutOfBound {
+    public static void main(String[] args) {
+        
+        int [] numbers = {1, 2, 3, 4, 5};
+        try{
+
+            System.out.println(numbers[10]); // This will throw ArrayIndexOutOfBoundsException
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Array index is out of bounds.");
+        }
+        System.out.println("Program continues after handling the exception.");
+    }
+
+}
