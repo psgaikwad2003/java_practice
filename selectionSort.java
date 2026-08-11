ad9cb84073def65619d4ae21ec2ce932ebf597e5
psgaikwad2003 <psgaikwad2003@gmail.com>
2026-08-11 14:12:04 +0530

    Add selectionSort.java - Selection Sort algorithm

---
+++ selectionSort.java
@@ -0,0 +1,33 @@
+public class selectionSort {
+    public static void main(String[] args) {
+
+        int[] numbers = {64, 25, 12, 22, 11};
+
+        System.out.println("Array before sorting:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        // Selection Sort Logic
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = numbers[minIndex];
+            numbers[minIndex] = numbers[i];
+            numbers[i] = temp;
+        }
+
+        System.out.println("\nArray after sorting (Selection Sort):");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
