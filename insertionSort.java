a7dc1a382accba21392790d7eaf6d8e1745fa52d
psgaikwad2003 <psgaikwad2003@gmail.com>
2026-08-11 14:12:08 +0530

    Add insertionSort.java - Insertion Sort algorithm

---
+++ insertionSort.java
@@ -0,0 +1,29 @@
+public class insertionSort {
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 11, 13, 5, 6};
+
+        System.out.println("Array before sorting:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        // Insertion Sort Logic
+        for (int i = 1; i < numbers.length; i++) {
+            int key = numbers[i];
+            int j = i - 1;
+
+            while (j >= 0 && numbers[j] > key) {
+                numbers[j + 1] = numbers[j];
+                j--;
+            }
+            numbers[j + 1] = key;
+        }
+
+        System.out.println("\nArray after sorting (Insertion Sort):");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
+    }
+}
