05c1097d758ccfa803275ba9b2c7d6314cf36aef
psgaikwad2003 <psgaikwad2003@gmail.com>
2026-08-30 19:36:54 +0530

    Add AtomicIntegerDemo to demonstrate atomic operations

---
+++ AtomicIntegerDemo.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerDemo {
+    private static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.incrementAndGet(); // Thread-safe atomic increment
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Counter Value: " + counter.get());
+    }
+}
