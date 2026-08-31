2b7c2dc43b51b96021fcd7c8b338a6b051c2b323
psgaikwad2003 <psgaikwad2003@gmail.com>
2026-08-31 21:27:00 +0530

    Add ConcurrentSkipListMapDemo demonstrating scalable concurrent maps

---
+++ ConcurrentSkipListMapDemo.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.ConcurrentSkipListMap;
+
+/**
+ * Demonstrates the usage of ConcurrentSkipListMap.
+ * It's a scalable concurrent ConcurrentNavigableMap implementation.
+ */
+public class ConcurrentSkipListMapDemo {
+    public static void main(String[] args) {
+        System.out.println("Initializing ConcurrentSkipListMap...");
+        ConcurrentSkipListMap<Integer, String> map = new ConcurrentSkipListMap<>();
+        
+        // Thread-safe operations
+        map.put(3, "Three");
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(4, "Four");
+
+        System.out.println("First entry: " + map.firstEntry());
+        System.out.println("Last entry: " + map.lastEntry());
+        
+        System.out.println("Iterating through the map:");
+        map.forEach((k, v) -> System.out.println("Key: " + k + ", Value: " + v));
+    }
+}
