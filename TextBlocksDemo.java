46eaf76bc997ace67be1fedc9cbf131c730ef112
psgaikwad2003 <psgaikwad2003@gmail.com>
2026-09-04 19:01:26 +0530

    Add TextBlocksDemo.java

---
+++ TextBlocksDemo.java
@@ -0,0 +1,13 @@
+public class TextBlocksDemo {
+    public static void main(String[] args) {
+        String json = """
+                {
+                    "name": "Jane Doe",
+                    "age": 28,
+                    "city": "New York"
+                }
+                """;
+                
+        System.out.println("JSON representation:\n" + json);
+    }
+}
